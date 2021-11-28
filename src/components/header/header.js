5abc01c461b083d798812ea6ddcf5315f791fb6e
Biloholovskyi <biloholovskyi>
2021-11-28 15:58:50 +0200

    Create app and create header and connect material ui

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import * as Style from './styled'
+
+const Header = () => {
+	return (
+		<Style.Wrapper>
+      <div className="title">React Test App</div>
+      <div className="subtitle">Biloholovskyi Danylo</div>
+		</Style.Wrapper>
+	)
+}
+
+export default Header
---
+++ src/components/header/styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Wrapper = styled.header`
+  padding: 12px 20px;
+  background-color: #1976d2;
+  
+  .title {
+    font-size: 24px;
+    color: #fff;
+  }
+  
+  .subtitle {
+    font-size: 18px;
+    color: #fff;
+    opacity: .7;
+    margin-top: 6px;
+  }
+`
+
+export {
+  Wrapper
+}
